7fad46e9ecb17f152d43f28c2e414c05a5b9bf37
marceloassuncao1020 <marceloassuncao1020@gmail.com>
2026-05-27 20:22:28 -0300

    Add files via upload

---
+++ calculadora.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "function_multiplicar.c"
+#include "function_dividir.c"
+    int main(){
+    int input = 0;
+    int numero1;
+    int numero2;
+
+    printf("Calculadora\n\n");
+
+    printf("1 - soma\n");
+
+    printf("2 - subtra�a�\n");
+
+    printf("3 - divis�o\n");
+
+    printf("4 - multiplica��o\n");
+
+    scanf("%d", &input);
+
+if(input == 1){
+        printf("\nDigite o priemiro numero: ");
+        scanf("%d", &numero1);
+        printf("\nDigite o segundo numero: ");
+        scanf("%d", &numero2);
+
+        printf("Resultado � :%d", numero1+numero2);
+    }
+
+if(input == 2){
+        printf("\nDigite o priemiro numero: ");
+        scanf("%d", &numero1);
+        printf("\nDigite o segundo numero: ");
+        scanf("%d", &numero2);
+
+        printf("Resultado � :%d", numero1-numero2);
+    }
+
+    if(input == 3){
+        printf("\nDigite o priemiro numero: ");
+        scanf("%d", &numero1);
+        printf("\nDigite o segundo numero: ");
+        scanf("%d", &numero2);
+
+        printf("Resultado � :%d", divisao(numero1, numero2));
+    }
+    if(input == 4){
+        printf("\nDigite o priemiro numero: ");
+        scanf("%d", &numero1);
+        printf("\nDigite o segundo numero: ");
+        scanf("%d", &numero2);
+
+        printf("Resultado � :%d", multiplicar(numero1, numero2));
+    }
+
+}
---
+++ function_dividir.c
@@ -0,0 +1,19 @@
+
+int divisao(int numero1, int numero2) {
+    
+    int dividendo, divisor;
+    int quociente, resto;
+
+    dividendo = numero1;
+    divisor = numero2;
+    quociente = 0;
+    resto = dividendo;
+
+    while (resto >= divisor) {
+        quociente = quociente + 1;
+        resto = resto - divisor;
+    }
+
+    printf("Quociente: %d\n", quociente);
+    printf("Resto: %d\n", resto);
+}
